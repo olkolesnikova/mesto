b58ace39eb2800f0afabe764bc61833241eaa9c3
olkolesnikova90 <olkolesnikova90@yandex.ru>
2023-03-20 03:23:39 +0500

    добавить открытие и закрытия popup

---
+++ index.js
@@ -0,0 +1,34 @@
+const editButton = document.querySelector(".profile__edit-button");
+const editProfileForm = document.querySelector(".popup");
+console.log(editProfileForm);
+const closeFormButton = editProfileForm.querySelector(".popup__close");
+const nameProfile = document.querySelector(".profile__name");
+const descriptionProfile = document.querySelector(".profile__description");
+const submitFormButton = editProfileForm.querySelector(".popup__submit");
+const nameInput = editProfileForm.querySelector(".popup__input_type_name");
+const descriptionInput = editProfileForm.querySelector(".popup__input_type_description");
+
+function openEditForm() {
+    editProfileForm.classList.add("popup_opened");
+    nameInput.value = nameProfile.innerHTML;
+    descriptionInput.value = descriptionProfile.innerHTML; 
+};
+
+function closeEditForm() {
+    editProfileForm.classList.remove("popup_opened");
+};
+
+function submitForm(evt) {
+    evt.preventDefault();
+
+    nameProfile.innerHTML = nameInput.value;
+    descriptionProfile.innerHTML = descriptionInput.value;
+    
+    editProfileForm.classList.remove("popup_opened");
+
+}
+
+editButton.addEventListener("click", openEditForm);
+closeFormButton.addEventListener("click", closeEditForm);
+editProfileForm.addEventListener("submit", submitForm);
+
